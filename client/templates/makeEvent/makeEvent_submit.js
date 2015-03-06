3af31fe56b0e793e1271cf6ce614985729f9a477
neilinglese <neilinglese@gmail.com>
2015-03-06 17:57:12 -0600

    fixed event decription

--- client/templates/makeEvent/makeEvent_submit.js
+++ client/templates/makeEvent/makeEvent_submit.js
@@ -1,9 +1,11 @@
 Template.makeEvent.events({
 	'submit form': function(e){
 		e.preventDefault();
+        var eventDescription = document.getElementById("eventDescription").value;
 		var newEvent = {
+
 			'eventName': $(e.target).parent().find('#eventName').val(),
-			'description': $(e.target).parent().find('#eventDescription').val(),
+			'description': eventDescription,
 			'targetMonth': $(e.target).parent().find('#monthPicked').val()
 		};
 		newEvent._id = Events.insert(newEvent);
