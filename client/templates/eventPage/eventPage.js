cba9c676cab054d8386865cc539362d425ef7718
maria-faulisi <mjfaulisi@gmail.com>
2015-03-17 14:06:17 -0500

    Delete event Page helper
    Data now being pulled from router

--- client/templates/eventPage/eventPage.js
+++ client/templates/eventPage/eventPage.js
@@ -1,12 +1,7 @@
 
 Template.eventPage.helpers({
-<<<<<<< HEAD
-	 currentEvent: function(){
-	 	return Events.find({_id: Session.get('newEventId')});
-	 }
-});
-=======
+
 });
 
 
->>>>>>> mf-second-branch
+
