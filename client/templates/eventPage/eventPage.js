94b6e8c88088e029400cbaa3af9d5a9b80e9e6ec
neilinglese <neilinglese@gmail.com>
2015-03-29 02:01:01 -0500

    updating eventPage.html and eventPage.js with new layout and calendar view

--- client/templates/eventPage/eventPage.js
+++ client/templates/eventPage/eventPage.js
@@ -1,7 +1,15 @@
-
+Template.eventPage.rendered = function(){
+    /*Getting Event data from iron-router and storing it into an array*/
+    var eventData = this.data.dates;
+    var calendar = $('#eventCalendar').fullCalendar({
+        /*dayRender function handling the intial rendering of days on page load*/
+        dayRender: function (date, cell) {
+            /*For each item in the eventData onLoad adding the toggleOn class*/
+            $.each(eventData,function(index,value){
+                $("td[data-date='"+value+"']").addClass('toggleOn');
+            });
+        }
+    })
+};
 Template.eventPage.helpers({
-
 });
-
-
-
