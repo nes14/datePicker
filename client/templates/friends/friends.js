c6061b2359ce8e548e5c9e2f781a7ca33419ea15
neilinglese <neilinglese@gmail.com>
2015-03-29 02:02:27 -0500

    begun implementing friends.html and friends.js

--- client/templates/friends/friends.js
+++ client/templates/friends/friends.js
@@ -1,5 +1,17 @@
-Template.profile.rendered = function() {
+Template.friends.rendered = function() {
 
 
 };
 
+theSearchResults = new Meteor.Collection('theSearchResults');
+
+Template.friends.events({'submit form' : function(event, template) {
+    event.preventDefault(); //prevent page reload
+    var email = event.target.Email.value;
+    var result = {
+        'result' : Meteor.users.find({ "emails.address" : email })
+    };
+
+    console.log(result);
+
+}});
