c4b763c3970fdea5175a635d2fe6a23058f99384
neilinglese <neilinglese@gmail.com>
2015-03-29 02:08:48 -0500

    implementing profile styles and update function clientside

--- client/templates/profile/profile.js
+++ client/templates/profile/profile.js
@@ -1,13 +1,22 @@
+
 Template.profile.rendered = function() {
 
 };
+
+
 Template.profile.events({
     'submit': function(event){
         event.preventDefault();
         var username = event.target.userName.value;
         var lastname = event.target.lastName.value;
+        var about = document.getElementById("about").value;
         var email = event.target.Email.value;
-        Meteor.users.update({_id:Meteor.user()._id}, {$set:{"profile.name":username}});
-        Meteor.users.update({_id:Meteor.user()._id}, {$set:{"profile.lastname":lastname}});
-        Meteor.users.update({_id:Meteor.user()._id}, {$set:{"emails":[{"address":email}]}});
-    }});
+        Meteor.call('updateemailfunction', email);
+        Meteor.users.update({_id:Meteor.user()._id}, {$set:{
+            "profile.name":username,
+            "profile.lastname":lastname,
+            "profile.about":about
+        }});
+    }
+});
+
