78fab7d31fcffa5b2abe8c087c0691557763c9c7
neilinglese <neilinglese@gmail.com>
2015-03-29 01:44:21 -0500

    collections

--- both/collections/events.js
+++ both/collections/events.js
@@ -3,10 +3,10 @@ Events = new Mongo.Collection('events');
 Events.allow({
 	insert: function(userId, doc){
 		return !! userId;
-	}, 
+	},
 	remove: function(userId, doc){
 		return !! userId;
-	}, 
+	},
 	update: function(userId, doc, fieldnames){
 		return !! userId;
 	}
