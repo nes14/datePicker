16cf42f0391a77563ac645f4249799648ec23f97
neilinglese <neilinglese@gmail.com>
2015-03-29 01:56:38 -0500

    updating dashboard.html and dashboard.js with new layout

--- client/templates/dashboard/dashboard.js
+++ client/templates/dashboard/dashboard.js
@@ -1,35 +1,50 @@
+
+Template.dashboard.rendered = function() {
+
+    if(Events.find({userId: Meteor.userId()}).count() === 0){
+        Session.set("hideButtons", false);
+    }else{
+        Session.set("hideButtons", true);
+        return Events.find({userId: Meteor.userId()});
+    }
+};
+
 Template.dashboard.helpers({
-	createdByUser: function(){
-		if(Events.find({userId: Meteor.userId()}).count() === 0){
-			var deleteBtn = $('.deleteEventBtn');
-			var editBtn = $('.editEventBtn');
-			deleteBtn.remove();
-			return [{eventName: "Oh noes!  You don't have any events yet, you should make one!"}];
-		}else{
-			return Events.find({userId: Meteor.userId()});
-		}
-	}
+
+    hidingButtons: function() { return Session.get('hideButtons'); },
+
+    createdByUser: function(){
+        if(Events.find({userId: Meteor.userId()}).count() === 0){
+            Session.set("hideButtons", false);
+        }else{
+            Session.set("hideButtons", true);
+            return Events.find({userId: Meteor.userId()});
+        }
+    }
 });
 
+
+
 Template.dashboard.events({
-	'click .eventLink': function(e){
-		e.preventDefault();
-		var thisId = this._id;
-		Session.set('newEventId', thisId);
-		Router.go('eventPage', {_id: thisId});
-	},
-	'click .editEventBtn': function(e){
-		e.preventDefault();
-		var thisId = this._id;
-		Router.go('editEvent', {_id: thisId});
-	},
-	'click .deleteEventBtn': function(e){
-		e.preventDefault();
-		var thisId = this._id;
-		var confirm = window.confirm("Are you sure you want to delete this event?");
-		if(confirm === true){
-			Events.remove({_id: thisId});
-		}
-	}
+
+    'click .eventLink': function(e){
+        e.preventDefault();
+        var thisId = this._id;
+        Session.set('newEventId', thisId);
+        Router.go('eventPage', {_id: thisId});
+    },
+    'click .editEventBtn': function(e){
+        e.preventDefault();
+        var thisId = this._id;
+        Router.go('editEvent', {_id: thisId});
+    },
+    'click .deleteEventBtn': function(e){
+        e.preventDefault();
+        var thisId = this._id;
+        var confirm = window.confirm("Are you sure you want to delete this event?");
+        if(confirm === true){
+            Events.remove({_id: thisId});
+        }
+    }
 });
 
